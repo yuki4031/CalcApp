6ab1d4ae4177cd36ea62300a5cd9822760b3b47d
Yuki Kawashima <yukikawashima1231@gmail.com>
2020-08-17 18:10:07 +0900

    課題提出用。

--- app/src/main/java/jp/techacademy/yuuki/kawashima/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuuki/kawashima/calcapp/MainActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.support.design.widget.Snackbar
 import kotlinx.android.synthetic.main.activity_result.*
+import android.util.Log
+import android.support.v7.app.AlertDialog
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity(),View.OnClickListener{
 
@@ -20,21 +23,33 @@ class MainActivity : AppCompatActivity(),View.OnClickListener{
         divide.setOnClickListener(this)
     }
 
+    private fun showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setMessage("数字を入力してください")
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
     override fun onClick(v: View) {
-        var num1 = editText1.toString().toDouble()
-        var num2 = editText2.toString().toDouble()
-        var result:String
-        if(v.id == R.id.plus){
-            result = (num1 + num2).toString()
-        }else if(v.id == R.id.minus){
-            result = (num1 - num2).toString()
-        }else if(v.id == R.id.multiply){
-            result = (num1 * num2).toString()
-        }else{
-            result = (num1 / num2).toString()
+        try {
+            val num1: Double = editText1.text.toString().toDouble()
+            val num2: Double = editText2.text.toString().toDouble()
+            var result: String
+            if (v.id == R.id.plus) {
+                result = (num1 + num2).toString()
+            } else if (v.id == R.id.minus) {
+                result = (num1 - num2).toString()
+            } else if (v.id == R.id.multiply) {
+                result = (num1 * num2).toString()
+            } else {
+                result = (num1 / num2).toString()
+            }
+            val intent = Intent(this, Result::class.java)
+            intent.putExtra("RESULT", result)
+            startActivity(intent)
+        }catch (e:Exception){
+            showAlertDialog()
         }
-        val intent = Intent(this,Result::class.java)
-        intent.putExtra("RESULT",result)
-        startActivity(intent)
     }
 }
